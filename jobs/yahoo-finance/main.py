e15b3afe65de8337f47533b25676f2101a65b65b
Louisalexis.Dubief <louisalexis.dubief@trafigura.com>
2023-03-29 11:16:07 +0200

    adding postgredb connection

--- jobs/yahoo-finance/main.py
+++ jobs/yahoo-finance/main.py
@@ -1,4 +1,11 @@
 import yfinance as yf
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext, SparkConf
+from awsglue.context import GlueContext
+from awsglue.job import Job
+import time
+from pyspark.sql.types import StructType, StructField, IntegerType, StringType
 
 tickers = {
     "Brent": "BZ=F",
@@ -25,20 +32,35 @@ tickers = {
 }
 
 if __name__ == "__main__":
-    data = yf.download(list(tickers.values()), period="max", interval="1d")
-    data = (
-        data.unstack(level=0)
-        .to_frame()
-        .reset_index()
-        .rename(
-            columns={
-                "level_0": "metric",
-                0: "value",
-                "level_1": "ticker",
-                "Date": "date",
-            }
-        )
-        .set_index(["date", "ticker", "metric"])
-        .sort_index()
-    )
-    data.dropna(inplace=True)
+    # data = yf.download(list(tickers.values()), period="max", interval="1d")
+    # data = (
+    #     data.unstack(level=0)
+    #     .to_frame()
+    #     .reset_index()
+    #     .rename(
+    #         columns={
+    #             "level_0": "metric",
+    #             0: "value",
+    #             "level_1": "ticker",
+    #             "Date": "date",
+    #         }
+    #     )
+    #     .set_index(["date", "ticker", "metric"])
+    #     .sort_index()
+    # )
+    # data.dropna(inplace=True)
+    sc = SparkContext()
+    glueContext = GlueContext(sc)
+    spark = glueContext.spark_session
+
+    connection_postgresql_options = {
+        "url": "jdbc:postgresql://ladubief-postgre-db-1.cywjasf2p0qq.us-east-1.rds.amazonaws.com:5432/ladubief",
+        "dbtable": "integration.dummy_data",
+        "user": "ladubief",
+        "password": "Dubief-74600"
+    }
+    df = glueContext.create_dynamic_frame.from_options(connection_type="postgresql",
+                                                          connection_options=connection_postgresql_options)
+    
+    print(df.head())
+    
